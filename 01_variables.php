d3fd9230dbd0371a3a1bf7fa9f40ccbd22bc7e3f
rncdrc <roncedric05@gmail.com>
2025-09-18 14:56:48 +0800

    added 01_variables.php and 02_strings.php

---
+++ 01_variables.php
@@ -0,0 +1,54 @@
+<?php
+
+$name = 'Zura';
+$age = '32';
+$isFather = 'true';
+$money = '19.99';
+$salary = 'null';
+
+// echo $name;
+// echo "Hello " . $name . "!<br>";
+// echo "Hello {$name}!<br>";
+// echo 'Hello {$name}!<br>';
+
+// echo $age."<br>";
+// echo $isFather."<br>";
+// echo $money."<br>";
+// echo $salary."<br>";
+
+// echo gettype($name)."<br>";
+// echo gettype($age)."<br>";
+// echo gettype($isFather)."<br>";
+// echo gettype($money)."<br>";
+// echo gettype($salary)."<br>";
+
+// print_r($name);
+// echo "<br>";
+
+// var_dump($name);
+// echo "<br>";
+
+// $name = 32;
+// echo gettype($name);
+// echo "<br>";
+
+// var_dump(is_string($name));
+// echo "<br>";
+// var_dump(is_string($age));
+// echo "<br>";
+// var_dump(is_int($age));
+// echo "<br>";
+// var_dump(is_bool($isFather));
+// echo "<br>";
+// var_dump(is_double($money));
+// echo "<br>";
+// var_dump(is_float($money));
+// echo "<br>";
+
+var_dump(isset($name));
+echo "<br>";
+var_dump(isset($country));
+echo "<br>";
+var_dump(isset($salary));
+
+?>
---
+++ 02_strings.php
@@ -0,0 +1,7 @@
+<?php
+
+$str = "Hello from PHP";
+echo strtolower($str)."<br>";
+echo strtoupper($str)."<br>";
+echo str_replace("PHP", "TheCodeholic", $str).'<br>';
+echo str_word_count($str).'<br>';
