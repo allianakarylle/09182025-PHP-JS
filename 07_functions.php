cf803a584a8053e087b9c736450341f0ca8b35cb
rncdrc <roncedric05@gmail.com>
2025-09-18 15:56:40 +0800

    added 07_functions.php

---
+++ 07_functions.php
@@ -0,0 +1,25 @@
+<?php
+
+// function sayHello() {
+//     echo "Hello, world!";
+// }
+
+// sayHello();
+
+// function greet($name) {
+//     echo "Hello, $name!";
+// }
+// greet("Alice");
+// greet("Bob");
+
+// function greet($name = 'Guest') {
+//     echo "Hello, $name!";
+// }
+// greet();
+// greet("Alice");
+
+function multiply($a, $b) {
+    return $a * $b;
+}
+$result = multiply(4, 3);
+echo $result;
