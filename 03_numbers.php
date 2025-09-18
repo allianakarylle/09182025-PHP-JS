a0f397b0962803006bf9edf05804bb90d173d997
rncdrc <roncedric05@gmail.com>
2025-09-18 15:07:47 +0800

    added 03_numbers.php

---
+++ 03_numbers.php
@@ -0,0 +1,36 @@
+<?php
+
+$x = 25;
+$y = 2;
+echo $x + $y;
+echo "<br>";
+
+$z = $x + $y;
+echo $z;
+echo "<br>";
+
+echo $x - $y;
+echo "<br>";
+echo $x * $y;
+echo "<br>";
+echo $x / $y;
+echo "<br>";
+echo $x % $y;
+echo "<br>";
+echo $x ** $y;
+echo "<br>";
+
+$y++;
+echo $y.'<br>';
+
+$y--;
+echo $y.'<br>';
+
+$y += 2;
+echo $y.'<br>';
+
+$y -= 2;
+echo $y.'<br>';
+
+echo min($x, $y).'<br>';
+echo max($x, $y).'<br>';
