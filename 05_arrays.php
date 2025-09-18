dbb3cac647d0dd7f79d2575a0a3b453920303e0b
rncdrc <roncedric05@gmail.com>
2025-09-18 15:48:04 +0800

    added 05_arrays.php and 06_loops.php

---
+++ 05_arrays.php
@@ -0,0 +1,26 @@
+<?php
+
+// $fruits = ["Apple", "Banana", "Cherry"];
+// echo $fruits[0] . '<br>';
+// $fruits[3] = 'Peach';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '<pre>';
+
+$mixedarray =[15, 'Apple', true];
+echo '<pre>';
+var_dump($mixedarray);
+echo '</pre>';
+
+$user = [
+    'name' => 'Zura',
+    'age' => 32,
+    'hobbies' => ['Coding', 'Tennis']
+];
+
+echo $user['name'] . '<br>';
+echo '<pre>';
+var_dump($user);
+echo '</pre>';
+
+
